38c59ab6179017e742b1cbc60f034ccfde6ba5ac
Maxim <myurachinskiy@edu.hse.ru>
2018-10-26 04:27:37 +0300

    Input values for all tasks

--- Linear_algebra_substitution_task/Program.cs
+++ Linear_algebra_substitution_task/Program.cs
@@ -7,10 +7,13 @@ namespace Linear_algebra_substitution_task
     {
         static void Main(string[] args)
         {
-            // string line = "2:1 i:5 4:p 1:3 7:4 j:p 5:8 k:7";
-            string line = "1:2 2:p r:3 3:s";
-            SubstitutionAssign assign = SubstitutionAssign.Positive;
-            // string line = "1:p 2:q 3:r";
+            // string line = "2:1 i:5 4:p 1:3 7:4 j:p 5:8 k:7"; // Task #1
+            // string line = "2:l 6:6 u:m 8:7 5:8 v:n 3:5 1:3"; // Task #2
+            // string line = "1:3 4:5 p:8 3:s q:t 7:4 8:7 r:1"; // Task #3
+            // string line = "3:6 5:2 6:1 2:x m:3 4:y 7:4 n:z"; // Task #4
+            // string line = "2:7 i:4 3:5 5:u 4:8 j:2 7:v k:3"; // Task #5
+            string line = "2:l 4:8 s:4 7:1 5:6 3:m 8:3 t:n"; // Task #6
+            SubstitutionAssign assign = SubstitutionAssign.Negative;
 
             var paramSubstitution = new ParamSubstitution(line);
 
